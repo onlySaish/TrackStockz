21e01cc4ecc73b64a7168be1fb28c66d1bc8e2ad
Saish More <onlysainok24@gmail.com>
2025-04-25 19:34:28 +0530

    Changed Name from Maverick To TrackStockz

--- src/features/dashboard/components/Navbar.jsx
+++ src/features/dashboard/components/Navbar.jsx
@@ -42,7 +42,7 @@ function Navbar() {
         </button>
         <div className="flex items-center gap-4">
           <div className="size-10 rounded-full bg-contain bg-no-repeat bg-left-bottom" style={{ backgroundImage: "url('reactLogo.webp')" }}></div>
-          <div className="text-2xl font-bold text-white">Maverick</div>
+          <div className="text-2xl font-bold text-white">TrackStockz</div>
         </div>
         <div className="flex items-center bg-gray-800 text-white rounded-full px-4 py-2 shadow-inner w-1/3">
           <Search className="text-gray-400" />
