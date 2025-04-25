7d55ad99a597a9b392b7060744fa7ea223488a07
Saish More <onlysainok24@gmail.com>
2025-04-25 20:17:55 +0530

    Production Backend Url update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy : {
-      "/api" : "http://localhost:8000"
+      "/api" : process.env.BACKEND_URL
     },
   },
   plugins: [react()],
